fc4c14bdd392ad98a79e00c734b9942cac8d0c64
snwfdhmp <snwfdhmp@gmail.com>
2023-04-14 02:07:19 +0200

    fix typo

--- main.js
+++ main.js
@@ -4,10 +4,10 @@
 	usage:
 		$ dapn bind <user> [--local <localIp>] [--remote <publicIp>]
       if the user ip is known, dapn will use it
-			else dapn will broadcast a bind request to every connected user, if the user knows the targeted user, they will forward the bind request to the user
-			when the ip is known, dapn will connect to the user and send a bind request
-			if the user accepts the bind request, each user will assign an ip to the user and will start forwarding traffic from and to the user on all exposed ports from the exported port list
-			if the user rejects the bind request, dapn will exit with an error
+      else dapn will broadcast a bind request to every connected user, if the user knows the targeted user, they will forward the bind request to the user
+      when the ip is known, dapn will connect to the user and send a bind request
+      if the user accepts the bind request, each user will assign an ip to the user and will start forwarding traffic from and to the user on all exposed ports from the exported port list
+      if the user rejects the bind request, dapn will exit with an error
 		$ dapn unbind <user>
 			dapn will notify user that their is no longer bound
 			dapn will disconnect from the user and will stop forwarding traffic from and to the user as well as stop assigning an ip to the user on all exposed ports from the exported port list
